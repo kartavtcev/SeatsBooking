797b9dc77542a4c3eedc56a0425d924812d16716
Kartavtsev <skartavt@t-systems.ru>
2021-07-19 21:03:37 +0300

    Init repo

---
+++ src/test/java/ApplicationTests.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ApplicationTests {
+    @Test
+    public void given_when_then() {
+
+        assertEquals(1,1);
+    }
+
+}
